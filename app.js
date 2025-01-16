febcc2d3c113078f207775622bb6b67f654513c6
Arthurzitos <arthurgruimaraes@gmail.com>
2025-01-16 00:13:23 -0300

    adicionando funcionalidades js

---
+++ app.js
@@ -0,0 +1,10 @@
+alert('Boas vindas ao jogo do número secreto');
+
+let numeroSecreto = 29;
+let tentativas = prompt('Escolha um número entre 1 e 30');
+
+if (tentativas == numeroSecreto) {
+    console.log('deu boa');
+} else () {
+    
+};
