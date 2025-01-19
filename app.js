8a37ad917a1cc16a5735eccddc8c8b730c500507
Arthurzitos <arthurgruimaraes@gmail.com>
2025-01-19 00:13:28 -0300

    concatenando string com variavel

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let numeroSecreto = 29;
 let tentativas = prompt('Escolha um número entre 1 e 30');
 
 if (tentativas == numeroSecreto) {
-    console.log('deu boa');
-} else () {
-    
+    alert(`Boa! Acertou o número secreto: ${numeroSecreto}`);
+} else {
+    alert('Número Incorreto.');
 };
